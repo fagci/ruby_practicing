9f3a19940819c16a4fb5690c5b8bd2f52647b330
Mikhail Yudin <fagci.nsk@gmail.com>
2021-11-05 21:06:23 +0700

    cleaner

--- bigrams.rb
+++ bigrams.rb
@@ -5,8 +5,7 @@ res = []
 ng_len = 2
 
 ARGF.read.downcase.split(/\W+/).map do |word|
-  wlen = word.length
-  (wlen - ng_len + 1).times do |i|
+  (word.length - ng_len + 1).times do |i|
     res.append(word[i, ng_len])
   end
 end
