a54b2916b25e68c1da46d13652b7fbbdc8f23805
Mikhail Yudin <fagci.nsk@gmail.com>
2021-11-05 16:40:14 +0700

    using standards

--- kub.rb
+++ kub.rb
@@ -1,10 +1,7 @@
 #!/usr/bin/env ruby
+# frozen_string_literal: true
 
-(102..987)
-  .each {
-    |n|
-    kub = n.digits.uniq.reverse
-    if kub.length==3 and kub.sum**3==n
-      puts "(" + kub.join("+") + ")^3=" + n.to_s
-    end
-}
+(102..987).each do |n|
+  kub = n.digits.uniq.reverse
+  puts "(#{kub.join('+')})^3=#{n}" if (kub.length == 3) && (kub.sum**3 == n)
+end
