22562451da8a0a6db694c836ce673ba8705aa990
Mikhail Yudin <fagci.nsk@gmail.com>
2022-07-07 22:21:16 +0700

    improvement

--- port_scanner.rb
+++ port_scanner.rb
@@ -4,7 +4,7 @@
 require 'socket'
 
 (1..1024).each do |port|
-  TCPSocket.new('192.168.0.200', port).close
+  Socket.tcp('192.168.0.200', port, connect_timeout: 0.75).close
   puts "#{port} open"
 rescue Errno::ECONNREFUSED, Errno::ETIMEDOUT
 end
